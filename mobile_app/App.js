2f0fac36abaa3c5f063a5c47ef70636120b52494
Tobi DEGNON <tobidegnon@proton.me>
2023-05-22 19:25:31 +0100

    initial commit

---
+++ mobile_app/App.js
@@ -0,0 +1,7 @@
+import './src/gesture-handler';
+import React from 'react';
+import Navigator from './src/Navigator';
+
+export default function App() {
+  return <Navigator />;
+}
---
+++ mobile_app/src/constants.js
@@ -0,0 +1,6 @@
+// This is the URL of your backend
+export const ENTRY_POINT_URL = 'http://127.0.0.1:8000/mobile';
+
+export const MAIN_STACK_NAME = 'Main';
+
+export const MODAL_STACK_NAME = 'Modal';
